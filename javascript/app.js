53c8b88f5ec63e2725b749b7f93e3666e921da54
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-03-07 00:57:42 +0500

    new changes

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,34 @@
 import jobs from "./data.js";
 
-// const template = document.getElementById(".job--list__template");
-// console.log(template);
-const listEl = document.querySelector("#list");
+const jobsList = document.getElementById("job--list");
+const jobsListItem = document.getElementsByClassName("job--list__item");
+const cloneNode = document.getElementById("job--list__template");
+// jobs.forEach((job) => {
+//   console.log(job);
+//   const clone = cloneNode.cloneNode(true);
+//   const cloneNode2 = clone.appendChild(cloneNode);
+
 jobs.forEach((job) => {
-  const newImgEl = document.createElement("img");
+  let temp = document.getElementsByTagName("template")[0];
+  let clone = temp.content.cloneNode(true);
+  console.log(clone);
+  clone.querySelector(".job--list__image").src = job.logo;
+  clone.querySelector(".job--list__company").textContent = job.company;
 
-  newImgEl.src = job.logo;
-  newImgEl.width = "88";
-  newImgEl.height = "88";
-
-  newLiEl.appendChild(newImgEl);
-
-  listEl.appendChild(newLiEl);
+  jobsList.appendChild(clone);
+  document.body.appendChild(clone);
 });
+{
+  /* <div class="job--list__filter">
+                  <span class="job--list__filter__item">Frontend</span>
+                  <span class="job--list__filter__item">Senior</span>
+                  <span class="job--list__filter__item">HTML</span>
+                  <span class="job--list__filter__item">CSS</span>
+                  <span class="job--list__filter__item">JavaScript</span>
+                </div> */
+}
+
+// jobs.forEach((job)=>{
+//   const filter = document.getElementsByClassName("job--list__filter");
+//   console.log(d)
+// }
